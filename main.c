7aa194001dbc4f8e87e19f5d5ec6461a8b7c0c5b
josu.igoa <josu.igoa@indaba.lks.es>
2023-02-27 07:46:26 +0100

    recursive menu creation

--- main.c
+++ main.c
@@ -22,6 +22,39 @@ static void tray_cb(struct tray_menu *item) {
   }
 }
 
+struct tray_menu* _create_menu(cJSON* json_menu) {
+  cJSON* item;
+  int menu_length = cJSON_GetArraySize(json_menu);
+  struct tray_menu* menu = (struct tray_menu *) malloc(sizeof(struct tray_menu) * (menu_length + 1));
+  for (int i = 0; i < menu_length; i++) {
+
+    item = cJSON_GetArrayItem(json_menu, i);
+    menu[i] = (struct tray_menu) {.text = cJSON_GetObjectItemCaseSensitive(item, "text")->valuestring, .cb = tray_cb};
+  
+    if (cJSON_HasObjectItem(item, "disabled")) {
+      menu[i].disabled = cJSON_GetObjectItemCaseSensitive(item, "disabled")->valueint;
+    }
+
+    if (cJSON_HasObjectItem(item, "checked")) {
+      menu[i].exit = cJSON_GetObjectItemCaseSensitive(item, "checked")->valueint;
+    }
+
+    if (cJSON_HasObjectItem(item, "checkbox")) {
+      menu[i].exit = cJSON_GetObjectItemCaseSensitive(item, "checkbox")->valueint;
+    }
+
+    if (cJSON_HasObjectItem(item, "exit")) {
+      menu[i].exit = cJSON_GetObjectItemCaseSensitive(item, "exit")->valueint;
+    }
+
+    if (cJSON_HasObjectItem(item, "menu")) {
+      menu[i].submenu = _create_menu(cJSON_GetObjectItemCaseSensitive(item, "menu"));
+    }
+  }
+  menu[menu_length] = (struct tray_menu) {.text = NULL};
+  return menu;
+}
+
 int main(int argc, char *argv[]) {
 
   if (argc < 2) {
@@ -29,8 +62,8 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  cJSON *menu_json = cJSON_Parse(argv[1]);
-  if (menu_json == NULL) {
+  cJSON *json_menu = cJSON_Parse(argv[1]);
+  if (json_menu == NULL) {
     const char *error_ptr = cJSON_GetErrorPtr();
     if (error_ptr != NULL) {
         fprintf(stderr, "Error before: %s\n", error_ptr);
@@ -38,62 +71,17 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  cJSON* item;
-  cJSON* text;
-  cJSON* menu;
-  cJSON* icon;
-  cJSON* sub_menu;
-  cJSON* sub_item;
-  cJSON* sub_text;
-
   char* icon_path;
 
-  icon = cJSON_GetObjectItemCaseSensitive(menu_json, "icon");
+  cJSON* icon = cJSON_GetObjectItemCaseSensitive(json_menu, "icon");
   if (cJSON_IsString(icon) && (icon->valuestring != NULL)) {
     icon_path = icon->valuestring;
   }
-  free(icon);
 
   struct tray ideckia_tray = { .icon = icon_path };
-
-  menu = cJSON_GetObjectItemCaseSensitive(menu_json, "menu");
-  int menu_length = cJSON_GetArraySize(menu);
-  ideckia_tray.menu = (struct tray_menu*) malloc(sizeof(struct tray_menu) * (menu_length + 1));
-  for (int i = 0; i < menu_length; i++) {
-    item = cJSON_GetArrayItem(menu, i);
-    ideckia_tray.menu[i] = (struct tray_menu) {.text = cJSON_GetObjectItemCaseSensitive(item, "text")->valuestring, .cb = tray_cb};
-    if (cJSON_HasObjectItem(item, "disabled")) {
-        ideckia_tray.menu[i].disabled = cJSON_GetObjectItemCaseSensitive(item, "disabled")->valueint;
-    }
-    if (cJSON_HasObjectItem(item, "exit")) {
-        ideckia_tray.menu[i].exit = cJSON_GetObjectItemCaseSensitive(item, "exit")->valueint;
-    }
-
-    if (cJSON_HasObjectItem(item, "sub")) {
-      sub_menu = cJSON_GetObjectItemCaseSensitive(item, "sub");
-      int sub_menu_length = cJSON_GetArraySize(sub_menu);
-      ideckia_tray.menu[i].submenu = (struct tray_menu *) malloc(sizeof(struct tray_menu) * (sub_menu_length + 1));
-
-      for (int j = 0; j < sub_menu_length; j++) {
-        sub_item = cJSON_GetArrayItem(sub_menu, j);
-        ideckia_tray.menu[i].submenu[j] = (struct tray_menu) {.text = cJSON_GetObjectItemCaseSensitive(sub_item, "text")->valuestring, .cb = tray_cb};
-        if (cJSON_HasObjectItem(item, "disabled")) {
-            ideckia_tray.menu[i].submenu[j].disabled = cJSON_GetObjectItemCaseSensitive(sub_item, "disabled")->valueint;
-        }
-        if (cJSON_HasObjectItem(item, "exit")) {
-            ideckia_tray.menu[i].submenu[j].exit = cJSON_GetObjectItemCaseSensitive(sub_item, "exit")->valueint;
-        }
-      }
-      ideckia_tray.menu[i].submenu[sub_menu_length] = (struct tray_menu) {.text = NULL};
-    }
-    
+  if (cJSON_HasObjectItem(json_menu, "menu")) {
+    ideckia_tray.menu = _create_menu(cJSON_GetObjectItemCaseSensitive(json_menu, "menu"));
   }
-
-  ideckia_tray.menu[menu_length] = (struct tray_menu) {.text = NULL};
-  free(menu);
-  free(item);
-  free(sub_menu);
-  free(sub_item);
 
   if (tray_init(&ideckia_tray) < 0) {
     printf("failed to create tray\n");
